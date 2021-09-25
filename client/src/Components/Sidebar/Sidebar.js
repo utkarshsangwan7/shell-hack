43128ed39dc768a6053c2a092210e2709708a7b7
Utkarsh Sangwan <utkarshsangwan7@gmail.com>
2021-09-25 19:35:38 +0530

    Compiler Page

---
+++ client/src/Components/Sidebar/Sidebar.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { Select,InputLabel,MenuItem} from '@mui/material';
+import './Sidebar.css';
+
+const Sidebar = ({language,theme,setLanguage,setTheme})=>{
+    const handleChangeLanguage = (e)=>{
+        console.log(e.target.value);
+        setLanguage(e.target.value);
+    }
+    const handleChangeTheme = (e)=>{
+        console.log(e.target.value);
+        setTheme(e.target.value);
+    }
+    return(
+        <div className='language-theme-option'>
+                <div>
+                    <InputLabel id="demo-simple-select-label">Language</InputLabel>
+                    <Select
+                        labelId="demo-simple-select-label"
+                        id="demo-simple-select"
+                        value={language}
+                        label="Language"
+                        onChange={handleChangeLanguage}
+                        className='select-option'
+                    >
+                        <MenuItem value={'c'}>C</MenuItem>
+                        <MenuItem value={'c++'}>C++</MenuItem>
+                        <MenuItem value={'java'}>Java</MenuItem>
+                        <MenuItem value={'python'}>Python</MenuItem>
+                        <MenuItem value={'javascript'}>JavaScript</MenuItem>
+                    </Select>
+                </div>
+                <div>
+                    <InputLabel id="demo-simple-select-label">Theme</InputLabel>
+                    <Select
+                        labelId="demo-simple-select-label"
+                        id="demo-simple-select"
+                        value={theme}
+                        label="Theme"
+                        onChange={handleChangeTheme}
+                        className='select-option'
+                    >
+                        <MenuItem value={'github'}>light</MenuItem>
+                        <MenuItem value={'monokai'}>dark</MenuItem>
+                    </Select>
+                </div>
+        </div>
+    );
+}
+
+export default Sidebar;
