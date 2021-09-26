d615a3c2a194da73034f729373731e3da75cb525
greatgabbar <strivedi_be19@thapar.edu>
2021-09-26 14:34:20 +0530

    integrate server backend

--- server.js
+++ server.js
@@ -2,14 +2,28 @@ const express= require('express');
 const socketio = require('socket.io');
 const http = require('http');
 const app= express();
+const cors=require('cors');
 const server = http.createServer(app);
 const io = socketio(server); 
 const port = process.env.PORT || 5000;
+require('dotenv').config();
 require('./sockets')(io);
 
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json());
+
+app.use(cors({ origin: `${process.env.CLIENT_URL}`, credentials: true }));
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", `${process.env.CLIENT_URL}`);
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 app.use('/code',require('./routes/code'));
 
--- test.c
+++ test.c
@@ -1,3 +1,9 @@
-#include<stdio.h> 
- int main() { printf("Hello World  GG Worlking");printf(" 
- ");}
+#include <stdio.h>
+
+int main(){
+  int x;
+  scanf("%d",&x);
+  printf("Value of x \n");
+  printf("%d \n",x);
+  return 0;
+}
